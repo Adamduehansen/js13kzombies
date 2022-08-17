7eba76885a6ace3925909fb9525ba475c32dccdd
Adam Due Hansen <adamduehan@gmail.com>
2022-08-17 06:56:32 +0200

    Uses js instead of ts.

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const context = document.getElementById('g').getContext('2d');
+
+function makeText(context) {
+  return {
+    text: '',
+    size: 32,
+    x: 0,
+    y: 0,
+    update: function (text, size, x, y) {
+      this.text = text;
+      this.size = size;
+      this.x = x;
+      this.y = y;
+    },
+    draw: function () {
+      context.font = `${this.size}px arial`;
+      context.fillStyle = 'white';
+      context.fillText(this.text, this.x, this.y);
+    },
+  };
+}
+
+const scene = {
+  enemies: 0,
+  zombies: 0,
+  enemiesText: makeText(context),
+  zombiesText: makeText(context),
+  update: function () {
+    this.enemiesText.update(`Enemies: ${this.enemies}`, 16, 10, 20);
+    this.zombiesText.update(`Zombies ${this.zombies}`, 16, 10, 64);
+  },
+  draw: function () {
+    this.enemiesText.draw();
+    this.zombiesText.draw();
+  },
+};
+
+function main() {
+  context.fillStyle = 'green';
+  context.fillRect(0, 0, 400, 800);
+  scene.update();
+  scene.draw();
+  requestAnimationFrame(main);
+}
+
+main();
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: path.resolve(__dirname, 'src/index.ts'),
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'game.js',
